9716c17d95e62d6cb328d3e4167efd6f54530593
Zuldajri <w.matyas15@hotmail.fr>
2020-06-08 17:00:36 +0200

    Update ocp4.sh

--- ocp4.sh
+++ ocp4.sh
@@ -38,3 +38,4 @@ sudo -i "s/RG-domain/$RG_DOMAIN/g" /root/openshift/install-config.yml
 sudo -i "s/location/$LOCATION/g" /root/openshift/install-config.yml
 sudo -i "s/pullSercet/$PULL_SECRET/g" /root/openshift/install-config.yml
 
+openshift-install create cluster --dir=/root/openshift --log-level=info
