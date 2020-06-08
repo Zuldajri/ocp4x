084965842d68abc6bfee3de5f55bf1a6d900ae3e
Zuldajri <w.matyas15@hotmail.fr>
2020-06-08 15:19:30 +0200

    Update ocp4.sh

--- ocp4.sh
+++ ocp4.sh
@@ -3,5 +3,26 @@
 echo $(date) " - Starting Master Script"
 
 ADMIN_USER=$1
+AZURE_CLIENT_ID=$2
+AZURE_TENANT_ID=$3
+AZURE_SUBSCRIPTION_ID=$4
+AZURE_CLIENT_SECRET=$5
 
-echo $USER >> /home/$ADMIN_USER/test
+sudo ssh-keygen -t rsa -b 4096 -N '' -f /home/root/.ssh/openshift
+sudo eval "$(ssh-agent -s)"
+sudo ssh-add /home/root/.ssh/openshift
+
+wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.4.7/openshift-client-linux-4.4.7.tar.gz
+tar xvf openshift-client-linux-4.4.6.tar.gz
+wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.4.7/openshift-install-linux-4.4.7.tar.gz
+tar xvf openshift-install-linux-4.4.6.tar.gz
+
+sudo mv oc kubectl openshift-install /usr/local/bin
+
+sudo mkdir /root/.azure
+sudo touch /root/.azure/osServicePrincipal.json
+sudo echo {"subscriptionId":"$AZURE_SUBSCRIPTION_ID","clientId":"$AZURE_CLIENT_ID","clientSecret":"$AZURE_CLIENT_SECRET","tenantId":"$AZURE_TENANT_ID"} > /root/.azure/osServicePrincipal.json
+
+sudo mkdir /root/openshift
+
+
