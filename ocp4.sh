94ad66f39a95eaddf194cc6821d82d5bda58067a
Zuldajri <w.matyas15@hotmail.fr>
2020-07-27 20:12:10 +0200

    Update ocp4.sh

--- ocp4.sh
+++ ocp4.sh
@@ -74,11 +74,16 @@ openshift-install create cluster --dir=openshift --log-level=info
 export KUBECONFIG=./openshift/auth/kubeconfig
 
 yum install -y httpd-tools
-sudo htpasswd -c -B -b ocppass $OPENSHIFT_USER $OPENSHIFT_PASSWORD
+wget https://raw.githubusercontent.com/Zuldajri/ocp4/master/oauth.yaml
+oc apply -f oauth.yaml
+
+htpasswd -c -B -b ocppass $OPENSHIFT_USER $OPENSHIFT_PASSWORD
 oc create secret generic htpass-secret --from-file=htpasswd=ocppass -n openshift-config
-sudo wget https://raw.githubusercontent.com/Zuldajri/ocp4/master/cr.yaml
+wget https://raw.githubusercontent.com/Zuldajri/ocp4/master/cr.yaml
 oc apply -f cr.yaml
+
 oc adm policy add-cluster-role-to-user cluster-admin $OPENSHIFT_USER
+oc adm policy add-role-to-user admin $OPENSHIFT_USER
 
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
